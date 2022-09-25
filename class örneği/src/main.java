e478648d157bc134ec1501db3c82833c84b067df
Mehmet Kardaş <mehmetgskmp31@gmail.com>
2022-09-25 18:47:18 +0300

    Classlara Örnek

---
+++ class örneği/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+	DortIslem dortIslem=new DortIslem();     // classı kullanmak için referans oluştur 
+	
+	int sonuc=dortIslem.topla(3,4);
+	System.out.println(sonuc);
+
+	}
+
+}
